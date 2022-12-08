9763963eb0f3ff30093df94a5b25942222377d26
Mahesh Jadhav <omkar28399@gmail.com>
2022-12-08 22:37:21 +0530

    recursive sum of n positive int updated

--- Recursion Problems/sumofpositiveint.py
+++ Recursion Problems/sumofpositiveint.py
@@ -1,7 +1,7 @@
 #Find sum of n positive numbers
 
 def sumofpositivenumbers(n):
-    assert n>=0 and int(n)==n, "Should be a positive integer"
+    assert n>=0 and int(n)==n, "Provide a positive integer only"
     if n==0:
         return 0
     else:
