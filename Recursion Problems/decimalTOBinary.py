f798bf1156c5649e7eb1251f608b9e8aad193618
Mahesh Jadhav <omkar28399@gmail.com>
2022-12-09 00:21:13 +0530

    DTB updated

--- Recursion Problems/decimalTOBinary.py
+++ Recursion Problems/decimalTOBinary.py
@@ -1,14 +1,9 @@
 #Conver decimal number to binary form
 
 def DTB(d):
+    assert int(d)==d,"Parameter should be integer decimal number"
     if d==0:
-        return '0'
-    elif d==1 :
-        return '1'
-    else:
-        if d%2==0:
-            return str(DTB(d//2))+str(0)
-        else:
-            return str(DTB(d//2))+str(1)
+        return 0
+    return d%2 + 10*DTB(int(d/2))
 
-print(DTB(456))
+print(DTB(-12))
