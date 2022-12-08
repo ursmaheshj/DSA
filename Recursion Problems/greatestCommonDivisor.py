e93bfbb050b1c431f789dd01d1395a564c78f8e2
Mahesh Jadhav <omkar28399@gmail.com>
2022-12-08 23:51:29 +0530

    GCD updated for negative values

--- Recursion Problems/greatestCommonDivisor.py
+++ Recursion Problems/greatestCommonDivisor.py
@@ -2,11 +2,13 @@
 
 def GCD(a,b):
     assert int(a)==a and int(b)==b,"Numbers must be integers only."
-    if 0 in [a,b]:
-        return max(a,b)
-    if a>b:
-        return GCD(a//b,a%b)
+    if a<0:
+        a=-a
+    if b<0:
+        b=-b
+    if b==0:
+        return a
     else:
-        return GCD(b//a,b%a)
+        return GCD(b,a%b)
 
-print(GCD(8,-12))
+print(GCD(18,48))
