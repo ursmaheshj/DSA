c810bcc864580fe0add37c0765c080624ba3664a
Mahesh Jadhav <omkar28399@gmail.com>
2022-12-08 23:08:12 +0530

    powerofnumber updated with negative exp

--- Recursion Problems/powerOfNumber.py
+++ Recursion Problems/powerOfNumber.py
@@ -1,10 +1,12 @@
 #Finding power/Exponential of a given number
 
 def power(base,exp):
-    assert exp>=0 and int(exp)==exp,"Exponential must be an positive integer only"
+    assert int(exp)==exp,"Exponential must be an positive integer only"
     if exp ==0:
         return 1
+    elif exp<0:
+        return 1/base * power(base,exp+1)
     else :
         return base*power(base,exp-1)
 
-print(power(-3,-3))
+print(power(-2,-3))
